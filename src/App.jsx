b93df16938f052054bc407c796f99e504810148c
AlexandrKisel <81587470+AlexandrKisel@users.noreply.github.com>
2021-10-27 22:04:06 +0300

    init app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+
+class App extends React.PureComponent {
+  render() {
+    return (
+      <div>
+        <h1>HELLO WORLD!</h1>
+      </div>
+    );
+  }
+}
+export default App;
